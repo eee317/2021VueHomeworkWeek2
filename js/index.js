bc74f6ce4087ee82270d7e143c6a2f6fb64b7619
Pei <gonsux.3686@gmail.com>
2022-01-21 17:26:44 +0800

    作業繳交

---
+++ js/index.js
@@ -0,0 +1,31 @@
+import {createApp} from 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.2.26/vue.esm-browser.min.js';
+const app={
+    data(){
+        return {
+            
+            user:{
+                username:'',
+                password:''
+            }
+        }
+    },
+    methods:{
+        singin(){
+            axios.post('https://vue3-course-api.hexschool.io/v2/admin/signin',this.user)
+            .then(res=>{
+                console.log(res)
+                const {token,expired}=res.data
+                document.cookie = `hexToken=${token}; expires=${expired}; path=/`;
+                window.location="products.html"
+            })
+            .catch(error=>{
+                console.dir(error)
+            })
+        }
+    },
+    mounted(){
+        
+    }
+}
+
+createApp(app).mount('#app')
---
+++ js/products.js
@@ -0,0 +1,46 @@
+import {createApp} from 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.2.26/vue.esm-browser.min.js';
+const app={
+    data(){
+        return {
+            url:'https://vue3-course-api.hexschool.io',
+            path:'peiying',
+            products:[],
+            tempProduct:{}
+        }
+    },
+    methods:{
+        check(){
+            const apiUrl=this.url+"/v2/api/user/check"
+            axios.post(apiUrl)
+            .then(res=>{
+                this.getData()
+            })
+            .catch(error=>{
+                console.dir(error)
+            })
+        },
+        getData(){
+            
+            const apiUrl=this.url+`/v2/api/${this.path}/admin/products`
+            axios.get(apiUrl)
+            .then(res=>{
+                this.products=res.data.products;
+                
+            })
+            .catch(error=>{
+                console.log(error)
+            })
+        },
+        click(item){
+            this.tempProduct=item;
+            
+        }
+    },
+    mounted(){
+        const token = document.cookie.replace(/(?:(?:^|.*;\s*)hexToken\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+        axios.defaults.headers.common['Authorization'] = token;
+        this.check();
+    }
+}
+
+createApp(app).mount('#app')
